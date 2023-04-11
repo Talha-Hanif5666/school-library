a445fd8a7a9da5cad11ba46e78afceb3060c1222
Talha-Hanif5666 <talhahanif5666@gmail.com>
2023-04-11 13:11:02 +0500

    Update linter errors

--- teachers.rb
+++ teachers.rb
@@ -2,7 +2,7 @@ require './persons'
 # Teacher class represents a teacher in a school.
 class Teacher < Person
   def initialize(age, specialization, name = 'unknown', parent_permission: true)
-    super(name, age, parent_permission:)
+    super(name, age, parent_permission)
     @specialization = specialization
   end
 
