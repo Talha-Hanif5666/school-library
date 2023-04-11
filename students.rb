5b36bddfa8f2a8670f8ebdc855a39800596b1313
Talha-Hanif5666 <talhahanif5666@gmail.com>
2023-04-11 13:05:05 +0500

    Adding classes

---
+++ students.rb
@@ -0,0 +1,13 @@
+require './persons'
+
+# Student class represents a student in a school.
+class Student < Person
+  def initialize(age, classroom, name = 'unknown', parent_permission: true)
+    super(name, age, parent_permission:)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯(ツ)/¯'
+  end
+end
